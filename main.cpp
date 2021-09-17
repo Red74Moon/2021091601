426753fa8dde5841d059c8606057db263506f41e
Red74Moon <fedoormaster@gmail.com>
2021-09-17 16:08:35 +0900

    삼각별 찍기     // *----     // **---     // ***--     // ****-     // *****

--- main.cpp
+++ main.cpp
@@ -6,22 +6,20 @@ using namespace std;
 
 int main()
 {   
-    // 배열
-    char C[10];
+    // *----
+    // **---
+    // ***--
+    // ****-
+    // *****
 
-    C[0] = 'H';
-    C[1] = 'e';
-    C[2] = 'l';
-    C[3] = 'l';
-    C[4] = 'o';
-    C[5] = '\0';  // 문자열의 끝을 알려준다. 없으면 이후로 쓰레기 값 나온다.
-
-
-    cout << C[0] << C[1] << C[2] << C[3] << C[4] << endl;
-
-    cout << C << endl;
-
-    string Hello = "Hello";  // 마지막에 \0 
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 
     return 0;  
 }
