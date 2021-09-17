b1abb29e48fd3424adbaaeb0876773cfd73a965a
Red74Moon <fedoormaster@gmail.com>
2021-09-17 13:27:00 +0900

    main 프로그램 변경

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ using namespace std;
 
 int main()
 {
-    int MyGold = 0;
-    float HP = 1.3234f;
-    char H = 'H';
+    int MyGold  = 0;
+    int MyChash = 0;
+    //float HP = 1.3234f;
+    //char H = 'H';
 
-    cout << MyGold << endl;
-    cout << HP << endl;
-    cout << H << endl;
+    //키보드 입력 받아서 MyGold에 저장
+    cout << "MyGold  = ";
+    cin >> MyGold;
+
+    cout << "MyChash = ";
+    cin >> MyChash;
+
+    //MyGold 내용을 모니터에 출력
+    cout << "MyGold + MyChash = " << MyGold + MyChash << endl;
+    //cout << HP << endl;
+    //cout << H << endl;
 
     return 0;  
 }
-
