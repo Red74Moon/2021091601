6650da35036fc1ec037216956ce05c646aff9760
Red74Moon <fedoormaster@gmail.com>
2021-09-17 15:40:12 +0900

    홀수의 합

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-    // 짝수의 합
+    // 홀수의 합
 
     int Total = 0;
   
-    for (int Count = 2; Count <= 100; Count = Count + 2) 
+    for (int Count = 1; Count <= 100; Count = Count + 2) 
     {
      Total = Total + Count;
     }
