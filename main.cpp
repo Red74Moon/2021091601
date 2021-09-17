db2fddebdc71224ace6bc0e57be35c29c87db047
Red74Moon <fedoormaster@gmail.com>
2021-09-17 16:01:18 +0900

    배열

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
 ﻿
 #include <iostream>
+#include <string> // C++ STL
 
 using namespace std;
 
 int main()
-{
-    // 3의 배수 합
+{   
+    // 배열
+    char C[10];
 
-    int Total = 0;
-  
-    for (int Count = 3; Count <= 100; Count = Count + 3) 
-    {
-     Total = Total + Count;
-    }
+    C[0] = 'H';
+    C[1] = 'e';
+    C[2] = 'l';
+    C[3] = 'l';
+    C[4] = 'o';
+    C[5] = '\0';  // 문자열의 끝을 알려준다. 없으면 이후로 쓰레기 값 나온다.
 
-    cout << Total << endl;
+
+    cout << C[0] << C[1] << C[2] << C[3] << C[4] << endl;
+
+    cout << C << endl;
+
+    string Hello = "Hello";  // 마지막에 \0 
 
     return 0;  
 }
